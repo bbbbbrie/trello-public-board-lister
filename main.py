195819dddb998b4ba2ca6b7244f2906c67e83e85
Brie Carranza <hi@brie.ninja>
2020-10-01 22:39:13 -0400

    Count number of boards found

--- main.py
+++ main.py
@@ -13,15 +13,18 @@ def get_public_trello_boards(user_name, api_key, api_token):
         ('token', api_token),
     )
     url = "https://api.trello.com/1/members/" + user_name + "/boards"
-    trello_boards = requests.get(url=url, params=my_parameters).text
+    response = requests.get(url=url, params=my_parameters)
+    trello_boards = response.text
+    number_of_boards = str(len(trello_boards))
+    json_body = response.json() 
     if len(trello_boards) > 2:
         filename = user_name + ".boards"
         with open(filename, mode="w") as trello_boards_file:
             full_path = os.path.abspath(filename)
-            loguru.logger.info("Writing down the public Trello boards for {} in {}", user_name, full_path, level="INFO")
             trello_boards_file.write(trello_boards)
     else:
-        loguru.logger.debug("No public Trello boards for {}", user_name, level="DEBUG")
+        loguru.logger.debug("{}: No public Trello boards for {}", user_name.upper(), user_name, level="DEBUG")
+    return json_body
 
 
 def get_api_keys_from_environment():
@@ -30,12 +33,21 @@ def get_api_keys_from_environment():
     return(api_key, api_token)
 
 
+def count_the_boards(json_body):
+    number_of_boards = int()
+    for i in json_body:
+        number_of_boards = number_of_boards + 1
+    return number_of_boards
+
+
 def main(user_name_list, api_key, api_token):
     if not (api_key and api_token):
         loguru.logger.error("No API key and token at the CLI; checking environment variables")
         api_key, api_token = get_api_keys_from_environment()
     for trello_user in set(user_name_list):
-        get_public_trello_boards(trello_user, api_key, api_token)
+        json_body = get_public_trello_boards(trello_user, api_key, api_token)
+        my_lists = count_the_boards(json_body)
+        loguru.logger.info("{}: I found {} public Trello boards while looking around https://trello.com/{}.", trello_user.upper(), my_lists, trello_user)
 
 
 if __name__ == "__main__":
